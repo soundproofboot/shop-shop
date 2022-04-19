7e2880782bca583d0ca600795d039442dc99e496
C Bares <colinedwinbares@gmail.com>
2022-04-19 11:18:32 -0500

    cart ui

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,28 +6,59 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+
+import Cart from '../components/Cart';
 
 function Detail() {
- const [state, dispatch] = useStoreContext();
- const { id } = useParams();
+  const [state, dispatch] = useStoreContext();
+  const { id } = useParams();
 
- const [currentProduct, setCurrentProduct] = useState({});
+  const [currentProduct, setCurrentProduct] = useState({});
 
- const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
- const { products } = state;
+  const { products, cart } = state;
 
- useEffect(() => {
-   if (products.length) {
-     setCurrentProduct(products.find((product) => product._id === id));
-   } else if (data) {
-     dispatch({
-       type: UPDATE_PRODUCTS,
-       products: data.products,
-     });
-   }
- }, [products, data, dispatch, id]);
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
+    });
+  };
+
+  useEffect(() => {
+    if (products.length) {
+      setCurrentProduct(products.find((product) => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+    }
+  }, [products, data, dispatch, id]);
   return (
     <>
       {currentProduct ? (
@@ -40,8 +71,13 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
@@ -51,6 +87,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
